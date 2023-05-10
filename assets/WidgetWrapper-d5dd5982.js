5b7041288aedc94c7c95253724d7dd91b8f96b57
scffs <khidey@inbox.ru>
2023-05-11 00:33:06 +0700

    Updates

---
+++ assets/WidgetWrapper-d5dd5982.js
@@ -0,0 +1,1 @@
+import{d as r}from"./Typography-ad9adadb.js";import{B as e}from"./useMediaQuery-5ff53dfc.js";const o=r(e)((({theme:r})=>({padding:"1.5rem 1.5rem 0.75rem 1.5rem",backgroundColor:r.palette.background.alt,borderRadius:"0.75rem"})));export{o as W};
