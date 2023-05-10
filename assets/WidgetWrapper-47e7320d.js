0a3c11bd1e160cb79711c931f06824865446c8d0
scffs <khidey@inbox.ru>
2023-05-11 00:20:33 +0700

    Updates

--- assets/WidgetWrapper-47e7320d.js
+++
@@ -1,1 +0,0 @@
-import{d as r}from"./Typography-ffbd0b37.js";import{B as e}from"./useMediaQuery-c853ae21.js";const o=r(e)((({theme:r})=>({padding:"1.5rem 1.5rem 0.75rem 1.5rem",backgroundColor:r.palette.background.alt,borderRadius:"0.75rem"})));export{o as W};
