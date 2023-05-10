654d77b8d714f60a8312c60a98cfff55b292cfd8
scffs <khidey@inbox.ru>
2023-05-10 23:58:36 +0700

    Updates

--- assets/UserImage-ebd199ec.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-7ffb1bb9.js";import{B as e}from"./useMediaQuery-37f9338f.js";const t=({image:t,size:i="60px"})=>s.jsx(e,{width:i,height:i,children:s.jsx("img",{style:{objectFit:"cover",borderRadius:"50%"},width:i,height:i,alt:"user",src:`http://localhost:3001/assets/${t}`})});export{t as U};
---
+++ assets/useDispatch-5cd579e2.js
@@ -0,0 +1,1 @@
+import{l as n,m as t,r as o}from"./index-aaf2ae70.js";function r(r=n){const s=r===n?t:()=>o.useContext(r);return function(){const{store:n}=s();return n}}const s=r();function c(t=n){const o=t===n?s:r(t);return function(){return o().dispatch}}const u=c();export{u};
